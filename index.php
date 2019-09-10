35b009501973a500f0f7bc79feb0241fbef44450
Sabir Shaykhlislamov <sha-sabir@yandex.ru>
2019-09-10 22:16:41 +0300

    some renamings

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Upload your files</title>
+ <meta charSet="utf-8"/>
+  <title>Загрузка файлов</title>
 </head>
 <body>
   <form enctype="multipart/form-data" action="index.php" method="POST">
-    <p>Upload your file</p>
+    <p>Загрузите ваш файл</p>
     <input type="file" name="uploaded_file"></input><br />
     <input type="submit" value="Upload"></input>
   </form>
@@ -18,8 +19,8 @@
     $path = $path . basename( $_FILES['uploaded_file']['name']);
 
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-      " has been uploaded";
+      echo "Файл ".  basename( $_FILES['uploaded_file']['name']).
+      " был загружен. Ссылка на файл: https://os.mipt.ru/files/" . basename( $_FILES['uploaded_file']['name']) ;
     } else{
         echo "There was an error uploading the file, please try again!";
     }
